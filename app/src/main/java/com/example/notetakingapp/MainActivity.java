6d18d67eb52d7ef939713ebaaa0421c1fae3c196
ggeorgekkariuki <gconsummer.gk@gmail.com>
2020-06-03 20:27:13 +0300

    Adding RecyclerView and Toolbar to MainActivity.java

--- app/src/main/java/com/example/notetakingapp/MainActivity.java
+++ app/src/main/java/com/example/notetakingapp/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.notetakingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar mToolbar;
+    RecyclerView mRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Reference and set up toolbar
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+//        References for the RecyclerView
+        mRecyclerView = findViewById(R.id.listOfNotes);
     }
 }
