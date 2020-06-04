e43551a80e74ace21875e0bfc60893fdb95620c4
ggeorgekkariuki <gconsummer.gk@gmail.com>
2020-06-04 20:18:57 +0300

    Adding a text listener to the note title edit text

--- app/src/main/java/com/example/notetakingapp/AddNoteActivity.java
+++ app/src/main/java/com/example/notetakingapp/AddNoteActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 
 public class AddNoteActivity extends AppCompatActivity {
@@ -16,10 +18,33 @@ public class AddNoteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_note);
 //        Set toolbar for second activity
         mToolbar = findViewById(R.id.toolbarSecondActivity);
+//        Display the toolbar
         setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("New Note");
 
 //        References to the EditTexts in the layout
         mEditTextNoteTitle = findViewById(R.id.editTextNoteTitle);
         mEditTextNoteContent = findViewById(R.id.editTextNoteContent);
+
+//        Listen to the Note Title edit text and change the toolbar to the title
+        mEditTextNoteTitle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence != null){
+//                    Change the Toolbar title when user inserts text in note title edit text
+                    getSupportActionBar().setTitle(charSequence);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
