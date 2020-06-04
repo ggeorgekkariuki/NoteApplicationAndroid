63252ba0de5e180907dc7a823841c3be86c470c2
ggeorgekkariuki <gconsummer.gk@gmail.com>
2020-06-04 19:22:12 +0300

    Inflating the Options Menu

--- app/src/main/java/com/example/notetakingapp/MainActivity.java
+++ app/src/main/java/com/example/notetakingapp/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar mToolbar;
@@ -18,5 +20,13 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
 //        References for the RecyclerView
         mRecyclerView = findViewById(R.id.listOfNotes);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        Register the menu and inflate it
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_menu, menu);
+        return true;
     }
 }
